3a19133f67d35d28cfc79373b35be5eb18349512
simon waronja <simon.waronja@student.moringaschool.com>
2022-06-28 15:47:35 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
-/* Your Code Here */
+function createEmployeeRecord(employeeArray) {
+    let testEmployee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return testEmployee;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+
+const getHour = function(dateTime) {
+    return parseInt(dateTime.match(/\d{4}$/)[0])
+}
+
+const getDate = function(dateTime) {
+    return dateTime.match(/\d{4}-\d{2}-\d{2}/)[0]
+}
+
+function createTimeInEvent(timeIn) {
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: getDate(timeIn),
+        hour: getHour(timeIn)
+    })
+    return this;
+}
+
+function createTimeOutEvent(timeOut) {
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: getDate(timeOut),
+        hour: getHour(timeOut)
+    })
+    return this;
+}
+
+function hoursWorkedOnDate(dateGiven) {
+    let timeIn = this.timeInEvents.find(event => 
+        event.date == dateGiven)
+    let timeOut = this.timeOutEvents.find(event => 
+        event.date == dateGiven)
+    let totalTime = (timeOut.hour - timeIn.hour) / 100
+    return totalTime;
+}
+
+function wagesEarnedOnDate(dateGiven) {
+    let hours = hoursWorkedOnDate.call(this, dateGiven)
+    return this.payPerHour * hours;
+}
+
+function findEmployeeByFirstName(array, name) {
+    return array.find(employee => employee.firstName == name)
+}
+
+function calculatePayroll(employeeRecord) {
+    return employeeRecord.reduce((total, employee) => {
+        return total + allWagesFor.call(employee)
+    }, 0)
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
